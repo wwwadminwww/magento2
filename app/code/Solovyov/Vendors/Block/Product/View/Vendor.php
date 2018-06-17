a021c288da8ae798aec73e14f18fd3211ecd34dd
Denis Solovyov <solovyov.denis@gmail.com>
2018-06-17 03:13:39 +0300

    show custom attribute on frontend

---
+++ app/code/Solovyov/Vendors/Block/Product/View/Vendor.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denissolovyov
+ * Date: 17.06.18
+ * Time: 01:23
+ */
+
+namespace Solovyov\Vendors\Block\Product\View;
+
+use Magento\Catalog\Block\Product\AbstractProduct;
+
+class Vendor extends AbstractProduct
+{
+
+}
